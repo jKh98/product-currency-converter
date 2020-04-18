d8203d800aaf973d086e42ec3c01686855eecc4e
jihad khurfan <jkhurfan.jk@gmail.com>
2020-04-19 02:23:22 +0300

    added price details dialog

--- app/src/main/java/com/jkhurfan/product_currency_converter/Activity/MainActivity.java
+++ app/src/main/java/com/jkhurfan/product_currency_converter/Activity/MainActivity.java
@@ -1,9 +1,14 @@
 package com.jkhurfan.product_currency_converter.Activity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,22 +17,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 import com.jkhurfan.product_currency_converter.DB.DatabaseHelper;
+import com.jkhurfan.product_currency_converter.DB.Product;
 import com.jkhurfan.product_currency_converter.R;
 
 import java.util.List;
 
 import info.androidhive.barcode.BarcodeReader;
 
-public class MainActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener {
+public class MainActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener, View.OnClickListener {
 
     BarcodeReader barcodeReader;
     TextView barcodeText;
+    Button getPriceBtn, addProductBtn;
+    DatabaseHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,30 +42,13 @@ public class MainActivity extends AppCompatActivity implements BarcodeReader.Bar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         barcodeText = findViewById(R.id.barcode);
+        getPriceBtn = findViewById(R.id.get_price_button);
+        addProductBtn = findViewById(R.id.add_new_product_button);
+        getPriceBtn.setOnClickListener(this);
+        addProductBtn.setOnClickListener(this);
 
-        DatabaseHelper helper = new DatabaseHelper();
-//        final TextView text = findViewById(R.id.test);
-
-        helper.getReference().addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                text.setText(dataSnapshot.child("bhlbk").getValue(Product.class).getName());
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e("getProduct", "The read failed: " + databaseError.getCode());
-            }
-        });
+        helper = new DatabaseHelper();
 
         // get the barcode reader instance
         barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_scanner);
@@ -90,4 +79,111 @@ public class MainActivity extends AppCompatActivity implements BarcodeReader.Bar
     public void onCameraPermissionDenied() {
         finish();
     }
+
+    @Override
+    public void onClick(View view) {
+
+        CharSequence barcodeString = barcodeText.getText();
+        switch (view.getId()) {
+            case R.id.get_price_button:
+                if (barcodeString != null && !barcodeString.toString().equals("")) {
+                    openPriceDialog(barcodeString.toString());
+                }
+                break;
+            case R.id.add_new_product_button:
+                if (barcodeString != null && !barcodeString.toString().equals("")) {
+                    openNewProductDialog(barcodeString.toString());
+                }
+                break;
+        }
+    }
+
+    private void openPriceDialog(final String s) {
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setTitle("Price Details");
+        dialog.setContentView(R.layout.price_details_dialog);
+
+        final TextView name = findViewById(R.id.name);
+        final EditText costUSD = findViewById(R.id.cost_usd);
+        final EditText costLBP = findViewById(R.id.cost_lbp);
+        final EditText rateUSD = findViewById(R.id.usd_rate);
+        final EditText profit = findViewById(R.id.profit_rate);
+        final TextView priceLBP = findViewById(R.id.selling_price_lbp);
+
+        helper.getReference().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Product product = dataSnapshot.child(s).getValue(Product.class);
+                if (product != null) {
+                    name.setText(product.getName());
+                    costUSD.setText(String.valueOf(product.getCost()));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("getProduct", "The read failed: " + databaseError.getCode());
+                findViewById(R.id.dialog_layout).setVisibility(View.GONE);
+            }
+        });
+
+        rateUSD.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                try {
+                    double rateUSDValue = Double.parseDouble(editable.toString());
+                    double costUSDValue = Double.parseDouble(costUSD.getText().toString());
+                    double profitRateValue = Double.parseDouble(profit.getText().toString());
+                    costLBP.setText(String.valueOf(rateUSDValue * costUSDValue));
+                    priceLBP.setText(String.valueOf(rateUSDValue * costUSDValue * (100 + profitRateValue) / 100));
+                } catch (Exception ignored) {
+                }
+            }
+        });
+
+        profit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                try {
+                    double rateUSDValue = Double.parseDouble(editable.toString());
+                    double costUSDValue = Double.parseDouble(costUSD.getText().toString());
+                    double profitRateValue = Double.parseDouble(profit.getText().toString());
+                    costLBP.setText(String.valueOf(rateUSDValue * costUSDValue));
+                    priceLBP.setText(String.valueOf(rateUSDValue * costUSDValue * (100 + profitRateValue) / 100));
+                } catch (Exception ignored) {
+                }
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void openNewProductDialog(String s) {
+        Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setTitle("Price Details");
+        dialog.setContentView(R.layout.new_product_dialog);
+
+        dialog.show();
+    }
+
 }
